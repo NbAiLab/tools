3fa6004887d6d0174635ef4978287c6698cedb4e
freddy <freddy.wetjen@gmail.com>
2020-09-29 08:57:37 +0200

    Version book 1.3

--- bookProcessor.py
+++ bookProcessor.py
@@ -10,5 +10,26 @@ from xmlHandler import xmlHandler
 from altoProcessor import altoProcessor
 
 if __name__ == '__main__':
-    bookProcessor=altoProcessor()
-    bookProcessor.ReadBook("../notram/testbok")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('listofbooks', help='File with path to all books')
+
+    args = parser.parse_args()
+       bookProcessor=altoProcessor()
+    bookProcessor.buildPublishedAndLanguageDict("publishedLangUrn.txt")
+
+    with open(args.listofbooks) as fp:
+        line=fp.readline().strip()
+        cnt=1
+        while line:
+            print(str(cnt) + "\t" + line)
+            res=bookProcessor.initBook(line)
+            print(res)
+            if res == True:
+                bookProcessor.ReadBook()
+                bookProcessor.printSingleFiles("tmp")
+                line = fp.readline().strip()
+                cnt+=1
+            else:
+                line = fp.readline().strip()
+                cnt += 1
+
